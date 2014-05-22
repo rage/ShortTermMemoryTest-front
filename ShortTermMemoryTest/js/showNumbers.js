d1e5a454eb8b345bb228d6367db88d29f589975b
madhaanr <marko.haanranta@helsinki.fi>
2014-05-22 17:11:02 +0300

    asks to return series

--- ShortTermMemoryTest/js/showNumbers.js
+++ ShortTermMemoryTest/js/showNumbers.js
@@ -11,34 +11,45 @@ var ShowNumbers = (function(evStore) {
     }
 
     console.log("Starting to show numbers");
-    var texts = {"normiSuunta":"Palauta sarja",
-                 "takaperin":"Palauta sarja käänteisessä järjestyksessä"};
-    var listOfNumbers = ["5","7","3","8","4","7","1"];
+    var texts = {normiSuunta:"Palauta sarja",
+                 takaperin:"Palauta sarja käänteisessä järjestyksessä"};
+    var listOfNumbers = ["5","7"/*,"3","8","4","7","1"*/];
     var sarjaLen = ["4","3"];
+    var order = "upwards";//["upwards","backwards"]"
     var index = 0;
 
-    function startShowing(listOfNumbers) {
+    function startShowing(/*listOfNumbers*/) {
         createHtml();
         showNumber();
+        //typeAnswer();
         function showNumber() {
-            //console.log("hiiii "+index)
-            $("h1").hide();
-            evStore.registerEvent("EVENT_START_NUMBER", "GAME_IDENTIFIER_BLAHBLAH", Date.now());
-            setTimeout(timeBetweenNumbers,500);
+            if(index<=listOfNumbers.length) {
+                $("h1").hide();
+                evStore.registerEvent("EVENT_START_NUMBER", "GAME_IDENTIFIER_BLAHBLAH", Date.now());
+                setTimeout(timeBetweenNumbers,500);
+            }
+            else {
+                $("h1").hide();
+                evStore.registerEvent("EVENT_END_TYPING", "GAME_IDENTIFIER_BLAHBLAH", Date.now());
+            }
         }
         function timeBetweenNumbers() {
             if(index<listOfNumbers.length) {
                 $("h1").show();
                 $("h1").html(listOfNumbers[index]);
                 index++;
-                evStore.registerEvent("EVENT_END_NUMBER", "GAME_IDENTIFIER_BLAHBLAH", Date.now());
                 setTimeout(showNumber,1000);
-                //console.log("h0000 "+index)
             }
+            else if(index==listOfNumbers.length) {
+                $("h1").show();
+                $("h1").html(texts.normiSuunta);
+                index++;
+                evStore.registerEvent("EVENT_START_TYPING", "GAME_IDENTIFIER_BLAHBLAH", Date.now());
+                setTimeout(showNumber,10000);
+            }
+            evStore.registerEvent("EVENT_END_NUMBER", "GAME_IDENTIFIER_BLAHBLAH", Date.now());
         }
-    }
-    function typeAnswer() {
-
+        console.log("here");
     }
 
     return {
