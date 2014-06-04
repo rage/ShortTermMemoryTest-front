51a0a4a90a8aab4dd385abd466da325f725eb443
madhaanr <marko.haanranta@helsinki.fi>
2014-06-04 16:03:42 +0300

    postResults test

--- ShortTermMemoryTest/js/postResults.js
+++ ShortTermMemoryTest/js/postResults.js
@@ -1,12 +1,10 @@
 function PostResults() {
 
     function post(results) {
-
-        var resultsJSON
         for(var i=0;i<results.length;i++) {
             results[i]["testcase_id"]=testcase_id;
         }
-        resultsJSON = {"testlog" : results};
+        var resultsJSON = {"testlog" : results};
         $.ajax({
             type: 'POST',
             url: url+"testlogs",
---
+++ test/postResultsTest.js
@@ -0,0 +1,32 @@
+/**
+ * Created by mhaanran on 4.6.2014.
+ */
+
+describe("PostResultsTest", function() {
+
+    beforeEach(function() {
+        testcase_id = 3;
+    });
+
+    it("postResults post() function called with results", function() {
+        var postResults = new PostResults();
+
+        var results = [{eventtype: "EVENT_GAME_START",
+                        timestamp: 1401882827646,
+                        value: "ThisGame" },
+                       {eventtype: "EVENT_SHOWLIST_START",
+                        timestamp: 1401882827646,
+                        value: ""}];
+        spyOn(postResults,"post");
+        postResults.post(results);
+        expect(postResults.post).toHaveBeenCalledWith(results);
+    });
+
+    
+
+    //var testJSON = {"testlog" : [{"testcase_id":"1", "eventtype":"dgffdsfdg", "value":"4", "timestamp":"122334223"},
+    //                               {"testcase_id":"1", "eventtype":"dgsfdg", "value":"4", "timestamp":"1223343"}]};
+
+
+
+});
