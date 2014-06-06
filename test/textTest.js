83a6b76f525eb559c36a6652cb7ff18cf7370741
Goasd <jonne.kari@gmail.com>
2014-06-06 15:23:09 +0300

    tests

---
+++ test/textTest.js
@@ -0,0 +1,46 @@
+
+describe("As a User, I want to see instructions", function() {
+    /*
+    var value
+
+    beforeEach(function(done) {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 1100000;
+
+        setTimeout(function() {
+            value = 0;
+            done();
+        }, 2);
+    });
+    it("instructions", function(done) {
+
+        var p = function simulateKeyPress(c) {
+            console.log(c)
+            jQuery.event.trigger({ type : 'keypress', which : c });
+        }
+
+        stateMachine.start();
+
+
+        document.getElementById('username').value = "Omena";
+        stateMachine.checkUsername("Omena")
+
+    });
+    it("instructions2", function(done) {
+        expect(document.getElementById("Notification").innerHTML).toBe(text["testinTarkoitus"]);
+
+        p(32);
+    });
+    it("instructions3", function(done) {
+        expect(document.getElementById("startScreen").innerHTML).toBe(text["ohje"]);
+
+        p(32);
+    });
+
+    it("instructions4", function(done) {
+        expect(document.getElementById("firstline").innerHTML).toBe(text["vaarin"]);
+
+        p(32);
+    });
+    */
+
+});
