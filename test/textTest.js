618cb6ec496dd4b77fd24ee0440b5ce7d65c44db
Goasd <jonne.kari@gmail.com>
2014-06-08 23:02:59 +0300

    textTest

--- test/textTest.js
+++ test/textTest.js
@@ -1,46 +1,55 @@
 
-describe("As a User, I want to see instructions", function() {
-    /*
-    var value
-
-    beforeEach(function(done) {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 1100000;
-
-        setTimeout(function() {
-            value = 0;
-            done();
-        }, 2);
+describe("As a new User, I want to see instructions", function() {
+  
+    beforeEach(function() {  
+        jasmine.clock().install();    
+        jasmine.clock().tick(200);
     });
+    
+    function simulateKeyPress(c) {
+        console.log(c)
+        jQuery.event.trigger({ type : 'keypress', which : c });
+    }
+
+    
     it("instructions", function(done) {
-
-        var p = function simulateKeyPress(c) {
-            console.log(c)
-            jQuery.event.trigger({ type : 'keypress', which : c });
-        }
-
+ 
         stateMachine.start();
 
 
         document.getElementById('username').value = "Omena";
-        stateMachine.checkUsername("Omena")
+        stateMachine.checkUsername("Omena") 
+		done();
 
     });
     it("instructions2", function(done) {
         expect(document.getElementById("Notification").innerHTML).toBe(text["testinTarkoitus"]);
 
-        p(32);
+        simulateKeyPress(32);
+        done();
+
     });
     it("instructions3", function(done) {
         expect(document.getElementById("startScreen").innerHTML).toBe(text["ohje"]);
 
-        p(32);
+        simulateKeyPress(32);
+        done();
+
     });
 
     it("instructions4", function(done) {
-        expect(document.getElementById("firstline").innerHTML).toBe(text["vaarin"]);
+        expect(document.getElementById("PracticeStart").innerHTML).toBe(text["harjoittelunAloitushje"]);
+        done();
 
-        p(32);
     });
-    */
+
+    it("instructions5", function(done) {
+
+        //simulateKeyPress(32); 
+        done();
+
+    });
+ 
+    
 
 });
