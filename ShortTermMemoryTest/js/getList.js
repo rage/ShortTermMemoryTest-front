c22dd80137f3aea153fd99c1f13f54c2e8adcea8
Goasd <jonne.kari@gmail.com>
2014-06-13 11:15:46 +0300

    getList

--- ShortTermMemoryTest/js/getList.js
+++ ShortTermMemoryTest/js/getList.js
@@ -1,31 +1,22 @@
 function GetList(){
 
     function getNextList(){
-
-        var req = new Request();
-        var params = "username="+username;
-        var urlEnd = "nextList";
-        var jsonData = req.createPost(url+urlEnd, params);
-
-        var jsonParsed = JSON.parse(jsonData);
-        var numberSets = jsonParsed["list"]["numbersets"];
-        testcase_id = jsonParsed["id"];
-
-        return createNumberList(numberSets);
-
+        return getList("nextList");
     }
 
 
     function getTrainingList(){
+        return getList("trainingList");
+    }
+
+    function getList(listType){
 
         var req = new Request();
         var params = "username="+username;
-        var urlEnd = "trainingList";
-        var jsonData = req.createPost(url+urlEnd, params);
+        var jsonData = req.createPost(url+listType, params);
 
         var jsonParsed = JSON.parse(jsonData);
         var numberSets = jsonParsed["list"]["numbersets"];
-
         testcase_id = jsonParsed["id"];
 
         return createNumberList(numberSets);
@@ -36,7 +27,7 @@ function GetList(){
         
         var numberList = [ ];
 
-        for(var i=0; i<list.length; i++) {
+        for(var i = 0; i < list.length; i++) {
             
             var numberSeries = [];
             var numbers = [];
@@ -47,7 +38,7 @@ function GetList(){
             
             numberSeries.order = list[i]["order"];
             numberSeries.numbers = numbers;
-            numberList[i]=numberSeries;
+            numberList[i] = numberSeries;
             
         }
 
