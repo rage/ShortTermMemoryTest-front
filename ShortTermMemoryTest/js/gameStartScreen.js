8b5b3098f7907841b86b54d10d52662fb7390153
Goasd <jonne.kari@gmail.com>
2014-06-13 13:56:47 +0300

    refactor

--- ShortTermMemoryTest/js/gameStartScreen.js
+++ ShortTermMemoryTest/js/gameStartScreen.js
@@ -24,16 +24,17 @@ function GameStartScreen(user){
 
     function createHtml(){
 
+        function createPages(type) {
+            document.body.innerHTML = '<div id="startScreen">' +
+                '<div id="startScreenP1">' + text[type+"1"] + '</div>' +
+                '<div id="startScreenP2">' + text[type+"2"] + '</div>' +
+                '<div id="startScreenP3">' + text[type+"3"] + '</div></div>';
+        }
+
         if(user.isTrained()){
-            document.body.innerHTML = '<div id="startScreen">' +
-                '<div id="startScreenP1">' + text["ohje1"] + '</div>' +
-                '<div id="startScreenP2">' + text["ohje2"] + '</div>' +
-                '<div id="startScreenP3">' + text["ohje3"] + '</div></div>';
+            createPages("ohje");
         }else{
-            document.body.innerHTML = '<div id="startScreen">' +
-                '<div id="startScreenP1">' + text["ohjeHarjoitteluSuorittamatta1"] + '</div>' +
-                '<div id="startScreenP2">' + text["ohjeHarjoitteluSuorittamatta2"] + '</div>' +
-                '<div id="startScreenP3">' + text["ohjeHarjoitteluSuorittamatta3"] + '</div></div>';
+            createPages("ohjeHarjoitteluSuorittamatta");
         }
 
     }
