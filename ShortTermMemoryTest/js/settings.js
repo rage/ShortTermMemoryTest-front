2cf41e7b3b921aa9cb29c6ffc8a0402d784318c7
Kaikkonen Arto M <artokaik@da1-cs-b221-27.helsinki.fi>
2014-06-16 15:22:04 +0300

    mahdollisuus kytkea sarjojen skippaaminen pois paalta

--- ShortTermMemoryTest/js/settings.js
+++ ShortTermMemoryTest/js/settings.js
@@ -22,14 +22,15 @@ var konsoli = function (){
 }();
 
 var gameSettings = {
-    numberDisplayTime: 100,
-    ISITime: 250,
-    guessTime: 500,
-    showResultTime: 5000,
+    numberDisplayTime: 1000,
+    ISITime: 1500,
+    guessTime: 10000,
+    showResultTime: 10000,
     showCrossDelay: 1000,
     showCrossTime: 500,
     maxPracticeRounds: 3,
     maxFails : 1, //Number of failed series allowed before the series of that length are dropped out
     failLimit : 2, //Correct numbers required in a series for series not to be considered a major fail
-    droppedSeriesMinLength : 6
+    droppedSeriesMinLength : 6,
+    droppingSeriesPossible: true
 }
